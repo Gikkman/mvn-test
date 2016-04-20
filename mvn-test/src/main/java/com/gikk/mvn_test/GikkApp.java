4e1099186cf1670fed6f0a22025f10c43696b08e
Gikkman <ksr.jonasson@gmail.com>
2016-04-20 05:39:05 +0200

    Added the source code

---
+++ mvn-test/src/main/java/com/gikk/mvn_test/GikkApp.java
@@ -0,0 +1,17 @@
+package com.gikk.mvn_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class GikkApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public static String getMessage(){
+    	return "Hello World";
+    }
+}
