6c9ffb854f07c2a1b60bde747727f9a6668d3291
Gikkman <ksr.jonasson@gmail.com>
2016-05-08 12:31:48 +0200

    Realized I wanted to test JavaDoc too

--- mvn-test/src/main/java/com/gikk/mvn_test/GikkApp.java
+++ mvn-test/src/main/java/com/gikk/mvn_test/GikkApp.java
@@ -6,11 +6,19 @@ package com.gikk.mvn_test;
  */
 public class GikkApp 
 {
+	/**Testing JavaDoc. See {@link GikkApp#getMessage()}
+	 * 
+	 * @param args these args
+	 */
     public static void main( String[] args )
     {
         System.out.println( "Hello World! This is a SnapShot" );
     }
     
+    /**More testing of JavaDocs. See {@link GikkApp}
+     * 
+     * @return a message
+     */
     public static String getMessage(){
     	return "Hello World 2! This is a SnapShot";
     }
